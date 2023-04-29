5ba693b5b564700ecfcc33812b3a7cf32a3f9acf
Gustavo <gustavoleandrogorges@gmail.com>
2023-04-28 23:45:52 -0300

    NOVAS FUNCOES FUNCIONANDO POREM DELETE NAO FUNCIONA AINDA

--- Python-Projects/crud_python_with_mysql/code/main.py
+++ Python-Projects/crud_python_with_mysql/code/main.py
@@ -11,118 +11,101 @@ mydb = mysql.connector.connect(
 
 # CRUD HOSPITAL
 
+#Criando hospital
 def create_hospital(nome, email, telefone,logradouro, cep, numero, bairro):
     
+    #Criando endereco para o hospital
     def create_endereco(logradouro, cep, numero, bairro):
         
         cursor = mydb.cursor()
-        sql = "INSERT INTO endereco (logradouro, cep, numero, bairro) VALUES (%s, %s, %s, %s)"
+        sql = "INSERT INTO endereco (logradouro, cep, numero, bairro) VALUES (%s, %s, %s, %s)" #Comando mysql
         val = (logradouro, cep, numero, bairro)
         cursor.execute(sql, val)
         mydb.commit()
         return cursor.lastrowid
     
-    create_endereco(logradouro, cep, numero, bairro)
+    create_endereco(logradouro, cep, numero, bairro) #Chamando a funcao que cria o endereco para o hospital
 
     cursor = mydb.cursor()
 
-    sql = "SELECT * FROM endereco ORDER BY id_endereco DESC LIMIT 1"
+    sql = "SELECT * FROM endereco ORDER BY id_endereco DESC LIMIT 1" #Selecionando o ID do endereco
     cursor.execute(sql)
     id_endereco = cursor.fetchone()
 
-    sql = "INSERT INTO hospital (nome, email, telefone, id_endereco) VALUES (%s, %s, %s,%s)"
+    sql = "INSERT INTO hospital (nome, email, telefone, id_endereco) VALUES (%s, %s, %s,%s)" #Criando o hospital, junto com o ID do respectivo endereco
     val = (nome, email,telefone,id_endereco[0],)
     cursor.execute(sql, val)
     mydb.commit()
     return cursor.lastrowid
 
+
+#Lendo informacoes da tabela hospital
 def read_hospital(id_hospital):
     cursor = mydb.cursor()
-    sql = "SELECT * FROM hospital WHERE id_hospital = %s"
+    sql = "SELECT * FROM hospital WHERE id_hospital = %s" #Comando mysql || pegando informacoes do hospital com ID
     val = (id_hospital,)
     cursor.execute(sql, val)
     result = cursor.fetchone()
 
-    sql = "SELECT * FROM endereco WHERE id_endereco = %s"
-    val = (result[0],)
+    sql = "SELECT * FROM endereco WHERE id_endereco = %s" #Comando mysql || pegando informacoes do endereco hospital
+    val = (result[4],)
     cursor.execute(sql, val)
     endereco = cursor.fetchone()
     
-    if result is None:
+    #Retornando informacoes
+    if result is None or endereco is None:
         return None
     return {'id': result[0], 'name': result[1], 'telefone': result[2], 'email': result[3],'logradouro':endereco[1],'cep':endereco[2],'numero':endereco[3],'bairro':endereco[4]}
 
+#Atuailizando informacoes hospital
 def update_hospital(id_hospital,nome, email, telefone,logradouro, cep, numero, bairro):
     cursor = mydb.cursor()
-    sql = "UPDATE hospital SET nome = %s, email = %s, telefone = %s WHERE id = %s"
+    sql = "UPDATE hospital SET nome = %s, email = %s, telefone = %s WHERE id_hospital = %s" #Comando mysql || atulizando dados
     val = (nome, email, telefone ,id_hospital)
     cursor.execute(sql, val)
     mydb.commit()
 
-    sql = "SELECT * FROM hospital WHERE id_hospital = %s"
+    sql = "SELECT * FROM hospital WHERE id_hospital = %s" #Selecionando ID endereco
     id = (id_hospital,)
-    cursor.execute(sql, id[4])
-    endereco = cursor.fetchone()
+    cursor.execute(sql, id)
+    endereco = cursor.fetchone() #Armazenando ID endereco
 
+    #Atuailizando informacoes endereco
     def update_endereco(logradouro, cep, numero, bairro,endereco):
+
+        #Comando mysql || Atuailizar informacoes endereco do hospital
         sql = "UPDATE endereco SET logradouro = %s, cep = %s, numero = %s, bairro = %s WHERE id_endereco = %s"
         val = (logradouro, cep, numero, bairro,endereco)
         cursor.execute(sql, val)
         mydb.commit()
         return cursor.rowcount
     
-    update_endereco(logradouro, cep, numero, bairro,endereco)
+    update_endereco(logradouro, cep, numero, bairro,endereco[4]) #Funcao que atualiza o endereco
 
     return cursor.rowcount
 
+#Deletando dados hospital
 def delete_hospital(id_hospital):
+    
     cursor = mydb.cursor()
-    sql = "DELETE FROM hospital WHERE id_hospital = %s"
+    sql = "SELECT * FROM hospital WHERE id_hospital = %s" #Selecionando ID endereco
+    id = (id_hospital,)
+    cursor.execute(sql, id)
+    id_endereco = cursor.fetchone() #Armazenando ID endereco
+
+    sql = "DELETE FROM endereco WHERE id_endereco = %s" #Deletando endereco || RECEBENDO ERRO POIS N POSSO DELETAR CHAVE ESTRANGEIRA
+    val = (id_endereco[4],)
+    cursor.execute(sql, val)
+    
+    cursor = mydb.cursor()
+    sql = "DELETE FROM hospital WHERE id_hospital = %s" #Deletando informacoes do hospital
     val = (id_hospital,)
     cursor.execute(sql, val)
     mydb.commit()
-    return cursor.rowcount
+    
 
-#CRUD ENDEREÇO
-
-
-def create_endereco(logradouro, cep, numero, bairro):
-    cursor = mydb.cursor()
-    sql = "INSERT INTO endereco (logradouro, cep, numero, bairro) VALUES (%s, %s, %s, %s)"
-    val = (logradouro, cep, numero, bairro)
-    cursor.execute(sql, val)
-    mydb.commit()
-    return cursor.lastrowid
-
-def read_endereco(id_endereco):
-    cursor = mydb.cursor()
-    sql = "SELECT * FROM endereco WHERE id_endereco = %s"
-    val = (id_endereco,)
-    cursor.execute(sql, val)
-    result = cursor.fetchone()
-    if result is None:
-        return None
-    return {'id': result[0], 'logradouro': result[1], 'cep': result[2], 'numero': result[3], 'bairro': result[4]}
-
-def update_endereco(id_endereco, logradouro, cep, numero, bairro):
-    cursor = mydb.cursor()
-    sql = "UPDATE endereco SET logradouro = %s, cep = %s, numero = %s, bairro = %s WHERE id = %s"
-    val = (logradouro, cep, numero, bairro,id_endereco)
-    cursor.execute(sql, val)
-    mydb.commit()
-    return cursor.rowcount
-
-def delete_endereco(id_endereco):
-    cursor = mydb.cursor()
-    sql = "DELETE FROM endereco WHERE id_endereco = %s"
-    val = (id_endereco,)
-    cursor.execute(sql, val)
-    mydb.commit()
+    
     return cursor.rowcount
 
 
 #Utilizando
-
-print(read_hospital(1))
-update_hospital("1","Jorge","jorge.com","1234","logradouro_poggers","1234","999","agro_é_tec")
-print(read_hospital(1))
