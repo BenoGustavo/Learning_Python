f792391221c005bc6e5f073689fee1f384d6e64d
Gustavo <gustavoleandrogorges@gmail.com>
2023-05-31 22:17:54 -0300

    Movie sorter project

--- Python-Projects/extras_testes/movie_sorter.py
+++ Python-Projects/extras_testes/movie_sorter.py
@@ -31,32 +31,43 @@ def main_window():
 
     #Setting up main window
     button_frame = tk.Frame(window, width=50, height=100)
-    button_frame.config(relief="raised",border=2,bg="#313232")
-    button_frame.pack(expand=True, fill='both', padx=50, pady=50,anchor='center')
+    button_frame.config(relief="flat",border=2,bg="white")
+    button_frame.pack(expand=True, fill='both', padx=50, pady=30,anchor='center')
 
     main_tittle = tk.Label(window, text="Movie sorter")
-    main_tittle.config(font=("roboto",12,"bold"),bg="grey",fg="white",relief="raised",border=0.5,padx=10,pady=10)
+    main_tittle.config(font=("roboto",12,"bold"),bg="#222222",fg="white",relief="raised",border=0.5,padx=10,pady=10)
 
-    button_mp3 = tk.Button(button_frame,text="Add movie")
-    button_mp3.config(font=("roboto",12,"bold"),width=25,height=3,anchor="center")
+    button_add_movie = tk.Button(button_frame,text="Add movie")
+    button_add_movie.config(font=("roboto",12,"bold"),bg="#222222",fg="white",width=25,height=3,anchor="center")
 
-    button_mp4 = tk.Button(button_frame,text="Delete movie")
-    button_mp4.config(font=("roboto",12,"bold"),width=25,height=3,anchor="center")
+    button_del_movie = tk.Button(button_frame,text="Delete movie")
+    button_del_movie.config(font=("roboto",12,"bold"),bg="#222222",fg="white",width=25,height=3,anchor="center")
 
-    button_mp4 = tk.Button(button_frame,text="Sort movie")
-    button_mp4.config(font=("roboto",12,"bold"),width=25,height=3,anchor="center")
+    button_sort_movie = tk.Button(button_frame,text="Sort movie")
+    button_sort_movie.config(font=("roboto",12,"bold"),bg="#222222",fg="white",width=25,height=3,anchor="center")
 
     #Packing itens
     main_tittle.pack(pady=20,side="top",anchor='n')
-    button_mp3.pack(pady=30)
-    button_mp4.pack(pady=25)
+    button_add_movie.pack(pady=15,anchor="center")
+    button_del_movie.pack(pady=10,anchor="center")
+    button_sort_movie.pack(pady=15,anchor="center")
 
     window.mainloop()
 
+def add_movie_window():
+    pass
+
+def del_movie_window():
+    pass
+
+def sort_movie_window():
+    pass
+
 # Setting up the window
 window = tk.Tk()
-window.config(background="#101111")
+window.config(background="#334464")
 window.title("Movie sorter")
 window.geometry("400x450")
 #
-    
+
+main_window()
