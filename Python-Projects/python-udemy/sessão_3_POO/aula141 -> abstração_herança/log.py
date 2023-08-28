fbfeaad39b585130d6ecd317dc0bd9a642e57b7c
BenoGustavo <gustavoleandrogorges@gmail.com>
2023-08-28 17:20:09 -0300

    entendendo um pouco sobre abstração e herança multipla

---
+++ Python-Projects/python-udemy/sessão_3_POO/aula141 -> abstração_herança/log.py
@@ -0,0 +1,34 @@
+from pathlib import Path
+
+LOG_FILE = Path(__file__).parent / 'log.txt'
+
+#Abstração
+class log:
+    def log(self,msg):
+        raise NotImplementedError('Create the log method yourself')
+    
+    def log_error(self,msg):
+        return self.log(f'Error:{msg}')
+    
+    def log_success(self,msg):
+        return self.log(f'Success:{msg}') #Quando utilizado self. dentro de uma classe que ira sofrer herança a classe que herda deve ser a chamada
+    
+class LogFileMixin(log):
+    def log(self,msg):
+        with open(LOG_FILE,'a') as arquivo:
+            arquivo.write(msg)
+            arquivo.write('\n')
+        
+    
+class LogPrintMixin(log):
+    def log(self,msg):
+        print(msg)
+
+if __name__ == '__main__':
+    lp = LogPrintMixin()
+    lp.log_error('Bom dia')
+    lp.log_success('Bacana')
+
+    lf = LogFileMixin()
+    lf.log_success('banana')
+    lf.log_error('ohhh no')
---
+++ Python-Projects/python-udemy/sessão_3_POO/aula141 -> abstração_herança/main.py
@@ -0,0 +1,7 @@
+from eletronico import Smartphone
+
+samsung = Smartphone('S22')
+iphone = Smartphone('13 pro max ultra blaster')
+
+samsung.ligar()
+iphone.ligar()
