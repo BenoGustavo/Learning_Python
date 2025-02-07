b9b01f7e41bb33442b5c0143a84957983b9fb80e
Gustavo <gustavoleandrogorges@gmail.com>
2025-02-07 12:44:43 -0300

    add initial implementations for factorial function and print statement in algorithm tests

---
+++ Python-Projects/extras_testes/tempCodeRunnerFile.py
@@ -0,0 +1,15 @@
+def FirstFactorial(num):
+
+#     fat = int(num)
+
+#     while True:
+#         fat -= 1
+        
+#         if fat == 0:
+#             break
+#         num = int(num)*int(fat)
+  
+#     return num
+
+# # keep this function call here 
+# print(FirstFactorial(input()))
